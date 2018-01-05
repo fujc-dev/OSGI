d7e59311ec3aa9ef58bd9c1b5aa09be101a38310
328969111@qq.com <fujun0824>
2018-01-05 21:54:52 +0800

    WPF特攻队内部资料

--- OSGi.NET/BundleRuntime.cs
+++ OSGi.NET/BundleRuntime.cs
@@ -5,71 +5,84 @@ using System.Linq;
 
 namespace OSGi.NET
 {
-    public class BundleRuntime : IDisposable
-    {
-        private static BundleRuntime _BundleRuntime = null;
-        private static Object lockObj = "lock";
+	public class BundleRuntime : IDisposable
+	{
+		private static BundleRuntime _BundleRuntime = null;
+		private static Object lockObj = "lock";
 
-        public static BundleRuntime Instance
-        {
-            get
-            {
-                lock (lockObj)
-                {
-                    if (_BundleRuntime == null)
-                    {
-                        _BundleRuntime = new BundleRuntime();
-                    }
-                    return _BundleRuntime;
-                }
-            }
-        }
+		public static BundleRuntime Instance
+		{
+			get
+			{
+				if (_BundleRuntime != null)
+				{
+					lock (lockObj)
+					{
+						if (_BundleRuntime == null)
+						{
+							_BundleRuntime = new BundleRuntime();
+						}
+					}
+				}
+				return _BundleRuntime;
+			}
+		}
 
-        public IFramework Framework { get; private set; }
+		public IFramework Framework { get; private set; }
 
-        /// <summary>
-        /// 启动OSGI.NET核心库
-        /// </summary>
-        public void Start()
-        {
-            var frameworkFactory = new FrameworkFactory();
-            Framework = frameworkFactory.CreateFramework();
-            Framework.Init();
-            Framework.Start();
-        }
+		/// <summary>
+		/// 启动OSGI.NET核心库
+		/// </summary>
+		public void Start()
+		{
+			var frameworkFactory = new FrameworkFactory();
+			Framework = frameworkFactory.CreateFramework();
+			Framework.Init();
+			Framework.Start();
+		}
 
-        /// <summary>
-        /// 以泛型方式根据服务约束类型获取服务
-        /// </summary>
-        /// <typeparam name="T">服务约束类型</typeparam>
-        /// <returns>服务实例</returns>
-        public Object GetFirstOrDefaultService(String contract)
-        {
-            var serviceReference = Framework.GetBundleContext().GetServiceReference(contract);
-            return serviceReference.GetService();
-        }
+		/// <summary>
+		/// 以泛型方式根据服务约束类型获取服务
+		/// </summary>
+		/// <typeparam name="T">服务约束类型</typeparam>
+		/// <returns>服务实例</returns>
+		public Object GetFirstOrDefaultService(String contract)
+		{
+			var serviceReference = Framework.GetBundleContext().GetServiceReference(contract);
+			return serviceReference.GetService();
+		}
 
-        public String GetObjectPropertyValue<T>(T t, string propertyname)
-        {
-            var type = typeof(T);
+		/// <summary>
+		/// 获取对象属性值
+		/// </summary>
+		/// <typeparam name="T">约束类型</typeparam>
+		/// <param name="t">对象</param>
+		/// <param name="propertyname">属性名称</param>
+		/// <returns></returns>
+		public String GetObjectPropertyValue<T>(T t, string propertyname)
+		{
+			var type = typeof(T);
 
-            var property = type.GetProperty(propertyname);
+			var property = type.GetProperty(propertyname);
 
-            if (property == null)
-            {
-                return string.Empty;
-            }
-            var o = property.GetValue(t, null);
+			if (property == null)
+			{
+				return string.Empty;
+			}
+			var o = property.GetValue(t, null);
 
-            if (o == null)
-            {
-                return string.Empty;
-            }
-            return o.ToString();
-        }
+			if (o == null)
+			{
+				return string.Empty;
+			}
+			return o.ToString();
+		}
 
-        public void Dispose()
-        {
-        }
-    }
+		#region IDisposable Interface Imp
+		public void Dispose()
+		{
+			//
+		}
+		#endregion
+	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 
-	开源OSGI.NET框架学习
+WPF特攻队内部资料
+
+项目结构
+MIS.ClientUI							//界面部分(程序界面，提示信息(弹出提示，右下角Tips提示等)
+MIS.YTIM								//即时通信部分(涉及到界面相关以及业务逻辑都会封装到此类库中，http://yanshi.sucaihuo.com/jquery/6/669/demo/)
+MIS.Foundation.Framework		//基础框架
+MIS.DB									//数据库处理部分(如果条件可行，会做一个Nhibernate、EF、ADO.NET等多库切换的封装处理)
+MIS.AutoUpdate						//更新程序，与服务器连接，并下载最新的DLL文件
+
+
+	MIS.Foundation.Framework侵入式框架：
+		顶部模块加载
+		左侧导航加载
+		Main内容部分加载方式
+		
+		核心：
+			1、动态加载插件（采用OSGI.NET，源码参见https://github.com/FreezeSoul/OSGi.NET） 
+				注：OSGi(Open Service Gateway Initiative)技术是Java动态化模块化系统的一系列规范。
+			2、DLL版本控制
+			3、即时通信Socket封装处理(采用SuperSocket框架来实现即时通信)
+			4、LocalMessagePusher(在框架类封装一个消息队列，用于生产者和消费者之间的通信的场景，暂时未定技术)
+			5、日志(Log4Net) 
+			6、AOP权限，日志，运行记录等(准备使用企业库)
 		
